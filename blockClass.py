f2856bf5b8a3e7121a5ddbc84c4d9d5f244961bd
Yash Patel <56071888+YashPatel1311@users.noreply.github.com>
2021-10-29 10:13:56 +0530

    block perfectly working as it should be

--- blockClass.py
+++ blockClass.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import hashlib
 import os
+import copy
 
 PREFIX_ZEROES = 4
 MAX_NONCE = 500000
@@ -21,6 +22,7 @@ class Block:
         self.flag = False
 
     def mine(self):
+        # st.write("mine called")
         prefix_str = '0'*PREFIX_ZEROES
         for nonce in range(MAX_NONCE):
             text = self.block_no + \
@@ -30,52 +32,69 @@ class Block:
             if hashh.startswith(prefix_str):
                 self.nonce = str(nonce)
                 self.hash = hashh
+                self.flag = False
+                self.hash_str=text
                 return
+
+
+    def hasChanged(self):
+
+        text = self.block_no + self.nonce + self.data
+
+        if text==self.hash_str:
+            self.flag=False
+
+        else:
+            self.flag=True
+
 
 
 def update_val(obj):
     obj.mine()
-    obj.flag = False
+    # st.warning(obj.hash)
 
 
 def touch(obj):
-    obj.flag = True
+    obj.block_no=st.session_state.block_no
+    obj.nonce=st.session_state.nonce
+    obj.data=st.session_state.data
+
+
 
 
 def block():
 
     obj = st.session_state.block
-    # st.session_state.obj
-    obj.block_no = st.text_input(
-        "Block #:", obj.block_no, on_change=touch, args=(obj,))
-    obj.nonce = st.text_input(
-        "Nonce:", obj.nonce, on_change=touch, args=(obj,))
-    obj.data = st.text_area("Data:", obj.data, on_change=touch, args=(obj,))
 
-    obj.hash_str = obj.block_no + obj.nonce + obj.data
+    st.text_input("Block #:",obj.block_no, key='block_no',on_change=touch,args=(obj,))
+    st.text_input("Nonce:",obj.nonce, key='nonce',on_change=touch,args=(obj,))
+    st.text_area("Data:",obj.data, key='data',on_change=touch,args=(obj,))
 
-    obj.hash = SHA256(obj.hash_str)
-    st.write("Hash:")
-    obj.hash
+
+    obj.hasChanged()
+    text = obj.block_no + obj.nonce + obj.data
+    obj.hash=SHA256(text)
+
+    # if st.button('Mine'):
+    #     update_val(obj)
 
     if obj.flag:
         st.error(obj.hash)
     else:
         st.success(obj.hash)
-        # st.balloons()
 
     st.button("Mine", on_click=update_val, args=(obj,))
 
-    # st.session_state.obj.block_no
-    # st.session_state.obj.nonce
-    # st.session_state.obj.data
-    # st.session_state.obj.hash_str
+    
 
 
 def main_block():
     if 'block' not in st.session_state:
+        st.session_state.default=[]
         st.session_state.block = Block()
         st.session_state.block.mine()
+
+
     block()
 
 
