4920b7034a4c6c3684ee8518e3340aeceb62e0b3
Yash Patel <56071888+YashPatel1311@users.noreply.github.com>
2021-11-03 13:31:40 +0530

    Chains are now rendered rendered independently

--- distributed.py
+++ distributed.py
@@ -8,30 +8,37 @@ class DistributedBlockchain():
     chain_cntr = 0
 
     def __init__(self):
-        self.node_counter = 1
         DistributedBlockchain.chain_cntr += 1
         st.write('Chain #', DistributedBlockchain.chain_cntr)
         prev = {}
         prev['hash'] = '0000000000000000000000000000000000000000000000000000000000000000'
         st.session_state.chain_network[DistributedBlockchain.chain_cntr] = {}
-        st.session_state.chain_network[DistributedBlockchain.chain_cntr][self.node_counter] = Blockchain(
-            prev, self.node_counter)
+        st.session_state.chain_network[DistributedBlockchain.chain_cntr][1] = Blockchain(
+            prev, 1)
 
-    def single_chain(self):
-        pass
+def single_chain(chain_ptr):
+
+    chain=st.session_state.chain_network[chain_ptr]
+    
+    ptr=1
+    while ptr<= len(chain):
+        chain[ptr].single_block()
+        ptr+=1
 
 
 def main_distributed():
 
+    
     if 'chain_network' not in st.session_state:
+
+        st.write('Chains not present')
+        st.session_state.chain_network = {}
+    else:
+        st.write('Chains present')
+
+    if DistributedBlockchain.chain_cntr == 0:
         for i in range(3):
             DistributedBlockchain()
-        st.warning('No chains created yet')
-        st.session_state.chain_network = {}
-
-    else:
-
-        st.write('Chains present')
 
     st.markdown("""
     <style>
@@ -51,12 +58,13 @@ def main_distributed():
 
     if peer == "Peer A":
         st.subheader("Peer A")
-        st.session_state['chain_network'][1]
+        # st.session_state['chain_network'][1]
+        single_chain(1)
 
     elif peer == "Peer B":
         st.subheader("Peer B")
-        st.session_state['chain_network'][2]
+        single_chain(2)
 
     elif peer == "Peer C":
         st.subheader("Peer C")
-        st.session_state['chain_network'][3]
+        single_chain(3)
